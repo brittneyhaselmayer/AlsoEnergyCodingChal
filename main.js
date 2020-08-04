84d09614744fee8f5315abb58d0b3f42f9a11907
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-08-04 16:40:28 -0500

    finished

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 /*****************************************************************************************
-* Part 2
-****************************************************************************************/
+ * Part 2
+ ****************************************************************************************/
 var employees = [
-        {first: "Amanda", last: "Byron", group: "Sales"},
-        {first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse"},
-        {first: "Miltiades", last: "Crescens", group: "Sales"},
-        /*...don't foget to account for other entries of the same form, but with different group names.....*/
-    ];
+	{ first: 'Amanda', last: 'Byron', group: 'Sales' },
+	{ first: 'Ye', last: 'Xia', group: 'Receiving', nameOrder: 'reverse' },
+	{ first: 'Miltiades', last: 'Crescens', group: 'Sales' },
+	/*...don't foget to account for other entries of the same form, but with different group names.....*/
+];
 
 // Part 2 Answer Here
 
+const func = (arr) => {
+	let sorted = {};
+
+	for (let i = 0; i < arr.length; i++) {
+		if (!sorted[arr[i].group] && !arr[i].nameOrder) {
+			sorted[arr[i].group] = [{ name: arr[i].first + ' ' + arr[i].last }];
+		} else if (!sorted[arr[i].group] && arr[i].nameOrder) {
+			sorted[arr[i].group] = [{ name: arr[i].last + ' ' + arr[i].first }];
+		} else if (sorted[arr[i].group] && arr[i].nameOrder) {
+			sorted[arr[i].group].push({
+				name: arr[i].last + ' ' + arr[i].first,
+			});
+		} else {
+			sorted[arr[i].group].push({
+				name: arr[i].first + ' ' + arr[i].last,
+			});
+		}
+	}
+	console.log(sorted);
+};
+
+func(employees);
 /*****************************************************************************************
-* Bonus
-****************************************************************************************/
+ * Bonus
+ ****************************************************************************************/
 
 // Bonus Anwser Here
